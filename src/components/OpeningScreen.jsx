9b2f4306ffa46d29f5923a01f39c0ed8eb15b92f
Gourav Patel <66197916+gouravpatel1611@users.noreply.github.com>
2026-03-21 22:50:13 +0530

    Update OpeningScreen.jsx

--- src/components/OpeningScreen.jsx
+++ src/components/OpeningScreen.jsx
@@ -1,8 +1,9 @@
-import title_img from "../assets/shubh_vivah.png";
 import head from "../assets/name.png"
 import { useEffect, useState } from "react";
 import Countdown from "./countDown";
 
+import Swastik from "./swastik"
+import ganeshji from "../assets/ganeshji.png"
 
 export default function OpeningScreen() {
   const [name, setName] = useState("");
@@ -14,21 +15,43 @@ export default function OpeningScreen() {
   }, []);
 
   return (
+    
     <div style={{ height: "100vh", display: "flex", flexDirection: "column",boxShadow: "0 0 50px rgba(0,0,0,0.5)" }}>
       {/* title Image */}
-      <img
-        src={title_img}
-        alt="logo"
-        style={{
-          position: "absolute",
-          top: "-25px",
-          left: "-25px",
-          width: "200px",
-        }}
-      />
+      
+      
+
+
+       <div style={{
+        display: "flex", flexDirection: "column", background: "rgba(0,0,0,0.50)"
+      }}>
+        <div style={{
+          fontSize: "5vw", fontWeight: "bold", textAlign: "center", color: "yellow"
+        }}>||  श्री गणेशाय नमः  ||</div>
+        <div style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: 'space-around',
+          fontSize: "3.5vw",
+          fontWeight: 'bold',
+          textAlign:"center"
+        }}>
+          <div><Swastik /></div>
+          <div>वक्रतुण्ड महाकाय निर्विघ्नं कुरु मे देव </div>
+          <div> <img src={ganeshji} alt="ganesh ji" style={{ width: "40px" }} /> </div>
+          <div>सूर्यकोटि समप्रभ : | सर्वकार्येषु सर्वदा ||</div>
+          <div> <Swastik /> </div>
+        </div>
+      </div>
+
+
+
+
+
+
       <div
         style={{
-          background: "rgba(0,0,0,0.30)",
+          background: "rgba(0,0,0,0.80)",
           width: "100vw",
           position: "absolute",
           top: "40vh",
@@ -50,7 +73,7 @@ export default function OpeningScreen() {
       >
         <div style={{ fontWeight: "bold" }}>प्रतिष्ठा में ,</div>
         <div style={{ marginLeft: "5vw" }}>आदरणीय <span style={{ fontSize: "6vw", fontWeight: "bold", marginLeft: "5vw" }}>{name}</span></div>
-      <div style={{ marginTop:"20px", padding:"5px" ,background: "rgba(0,0,0,0.80)",borderRadius: "40px",boxShadow: "0 0 50px rgba(0,0,0,0.3)"}}>
+      <div style={{ marginTop:"10px", marginBottom:"30px", padding:"5px" ,background: "rgba(0,0,0,0.80)",borderRadius: "40px",boxShadow: "0 0 50px rgba(0,0,0,0.3)"}}>
         <Countdown/>
       </div>
       </div>
